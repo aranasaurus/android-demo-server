9877e367e3eddbd56f573f3853a16c4f7476f519
Ryan Arana <ryan.arana@gmail.com>
2013-01-26 19:59:42 -0800

    functional?

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 import os
-from flask import Flask
+import time
+import json
+import requests
+from requests.exceptions import ConnectionError
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def go():
+    query = "picard smartphone"
+    url = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q={0}"
+
+    r = requests.get(url.format(query))
+    images = [(i, json.dumps(i)) for i in json.loads(r.text)['responseData']['results']]
+    return render_template("index.html", images=images)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
