7d9150586f4ec0cb162aa91932eafda3c6dae81b
Ryan Arana <ryan.arana@gmail.com>
2013-01-27 01:24:16 -0800

    add the ability to set the query without a deploy =P

--- app.py
+++ app.py
@@ -11,12 +11,20 @@ app.debug = True
 
 @app.route('/')
 def go():
-    query = "this is data"
+    if os.path.exists('query.txt'):
+        query = open('query.txt', 'r').read()
+    else:
+        query = "picard android"
     url = "http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q={0}"
 
     r = requests.get(url.format(query))
     images = [(i, json.dumps(i)) for i in json.loads(r.text)['responseData']['results']]
     return render_template("index.html", images=images)
+
+@app.route('/set_query/<query>')
+def write_query(query):
+    open('query.txt', 'w').write(query)
+    return 'Query set to "{0}"'.format(query)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
